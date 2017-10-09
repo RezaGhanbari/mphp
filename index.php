7514456e555d77f0400e163a8a5d3cdf7a6a9b8b
Reza Ghanbari <rezapython@gmail.com>
2017-10-09 09:40:57 +0330

    front_file_added

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
+require_once __DIR__.'/init.php';
 
-require_once __DIR__.'/vendor/autoload.php';
+$input = $request->get('name', 'World');
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
-
-$input = $request->get('name', 'world');
-
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'utf-8')));
-
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->send();
